013765e77af122e8fd35e9c6b9185568377b2b92
wycarol44 <wycarol44@gmail.com>
2026-06-11 21:44:05 -0400

    update the instruction to match with the difficulty range

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if "show_hint" not in st.session_state:
 st.subheader("Make a guess")
 
 st.info(
-    f"Guess a number between 1 and 100. "
+    f"Guess a number between {low} and {high}. "
     f"Attempts left: {attempt_limit - st.session_state.attempts}"
 )
 
