5f363e6c110bbc620db6218fe649873ab7430b1a
wycarol44 <wycarol44@gmail.com>
2026-06-11 21:36:58 -0400

    update the gange to match with difficulty, the new game senario should clean up history and score

--- app.py
+++ app.py
@@ -114,8 +114,10 @@ with col3:
 
 if new_game:
     st.session_state.attempts = 0
-    st.session_state.secret = random.randint(1, 100)
+    st.session_state.secret = random.randint(low, high)
     st.session_state.status = "playing"
+    st.session_state.history = []
+    st.session_state.score = 0
     st.success("New game started.")
     st.rerun()
 
